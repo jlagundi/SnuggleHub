f6314680e9c83829963f3ebde176697cbc071ea9
jlagundi <81765566+jlagundi@users.noreply.github.com>
2023-10-23 03:12:01 +0800

    version 2

--- SnuggleHub_Bulk.ps1
+++ SnuggleHub_Bulk.ps1
@@ -5,36 +5,26 @@ $ListName = "Pets"
 $csvContent = Get-Content -Path 'C:\SnuggleHub_Repo\pets_data.csv' -TotalCount 1
 $delimiter = ($csvContent | Select-String -Pattern '\s').Matches.Count
 
-
-# Connect to SharePoint Online using MFA
 Connect-PnPOnline -Url $SiteURL -Interactive
 
-# Get the CSV file data
 $CSVData = Import-Csv -Path $CSVFilePath
 
-# Iterate through each row in the CSV
 foreach ($Item in $CSVData) {
-    # The query seems to be incorrectly formatted. Ensure the CAML query is accurate for your list structure.
     $Query = "<View><Query><Where><And><Eq><FieldRef Name='Title'/><Value Type='Text'>$($Item.Name)</Value></Eq><Eq><FieldRef Name='ImageURL'/><Value Type='Text'>$($Item.ImageURL)</Value></Eq></And></Where></Query></View>"
-    # Consider revising the query if it's not providing the expected results.
-
-    # Use try-catch block to handle any errors that may occur during the process.
     try {
         $Items = Get-PnPListItem -List $ListName -Query $Query
     }
     catch {
         Write-Host "Error occurred while retrieving list items: $_.Exception.Message"
-        continue  # Skip to the next iteration if an error occurs
+        continue
     }
 
     if ($Items.Count -eq 0) {
         $ItemProperties = @{
             "Title" = $Item.Name
-            "ImageURL" = $Item.ImageURL  # Adjusted property name
-            # Add more properties as needed
+            "ImageURL" = $Item.ImageURL
         }
 
-        # Add item to SharePoint Online list
         Add-PnPListItem -List $ListName -Values $ItemProperties
 
         Write-Host "Item created: $($Item.Name)"
@@ -43,5 +33,4 @@ foreach ($Item in $CSVData) {
     }
 }
 
-# Disconnect from the SharePoint site
 Disconnect-PnPOnline
